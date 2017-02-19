397eaa8633e29de83d7126318a993b0400e9b93d
Dave Arroyo <arroyo.daveil@gmail.com>
2017-02-19 16:15:52 +0800

    Inital jewelry builder

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Tara Jewelry Builder</title>
 	<link href="libs/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/tara.css" />
 	<!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -15,11 +16,21 @@
     <div class="container">
 		<div class="row">
 			<div class="col-md-10 col-md-offset-1">
-				Welcome to Tara!
+				<center>
+				<h2>Welcome to Tara!</h2>
+				<button class="btn btn-default add-pendant" data-pendant="A">+ A</button>
+				<button class="btn btn-default add-pendant" data-pendant="B">+ B</button>
+				<button class="btn btn-default add-pendant" data-pendant="C">+ C</button>
+				<button class="btn btn-default" id="add-random-pendant">Random</button>
+				<button class="btn btn-warning" id="undo-pendant">Undo</button>
+				<button class="btn btn-danger" id="clear-pendants">Clear</button>
+				</center>
 			</div>
 		</div>
 	</div>
     <script src="libs/bower_components/jquery/dist/jquery.min.js"></script>
     <script src="libs/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="libs/bower_components/pixi.js/dist/pixi.min.js"></script>
+	<script type="text/javascript" src="js/tara.js"></script>
   </body>
 </html>
