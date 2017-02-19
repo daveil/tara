33dbb1d7e1182f65b24a41791657918774ed9dda
Dave Arroyo <arroyo.daveil@gmail.com>
2017-02-19 14:28:46 +0800

    Init project assets

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tara Jewelry Builder</title>
+	<link href="libs/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+	<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				Welcome to Tara!
+			</div>
+		</div>
+	</div>
+    <script src="libs/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="libs/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
