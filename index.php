3ba1e1366ce9620eccb1592d3067f97e5e8771d3
Dave Alaras <arroyo.daveil@gmail.com>
2017-03-19 18:48:57 +0800

    Update ui structure

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Tara Jewelry Builder</title>
 	<link href="libs/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="css/tara.css" />
+	<link rel="stylesheet" href="css/tara.css<?php echo '?a='.rand();?>" />
 	<!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -14,23 +14,74 @@
   </head>
   <body>
     <div class="container">
-		<div class="row">
-			<div class="col-md-10 col-md-offset-1">
-				<center>
-				<h2>Welcome to Tara!</h2>
-				<button class="btn btn-default add-pendant" data-pendant="a">+ A</button>
-				<button class="btn btn-default add-pendant" data-pendant="b">+ B</button>
-				<button class="btn btn-default add-pendant" data-pendant="c">+ C</button>
-				<button class="btn btn-default" id="add-random-pendant">Random</button>
-				<button class="btn btn-warning" id="undo-pendant">Undo</button>
-				<button class="btn btn-danger" id="clear-pendants">Clear</button>
-				</center>
-			</div>
+		<div id="jde-canvas"></div>
+		<div id="jde-actions"></div>
+		<div class="jde-horizontal-wrapper">
+			<ul class="jde-ui-list">
+				<li class="jde-ui-item">
+					<div class="jde-jewel">
+						<div class="jde-img"></div>
+						<div class="jde-title">ITEM 1</div>
+						<button class="jde-btn btn btn-xs">ADD</button>
+					</div>
+				</li>
+				<li class="jde-ui-item">
+					<div></div>
+				</li>
+				<li class="jde-ui-item">
+					<div></div>
+				</li>
+				<li class="jde-ui-item">
+					<div></div>
+				</li>
+				<li class="jde-ui-item">
+					<div></div>
+				</li>
+				<li class="jde-ui-item">
+					<div></div>
+				</li>
+				<li class="jde-ui-item">
+					<div></div>
+				</li>
+				<li class="jde-ui-item">
+					<div></div>
+				</li>
+				<li class="jde-ui-item">
+					<div></div>
+				</li>
+				<li class="jde-ui-item">
+					<div>
+						
+					</div>
+				</li>
+				<li class="jde-ui-item">
+					<div>
+						
+					</div>
+				</li>
+				<li class="jde-ui-item">
+					<div>
+						
+					</div>
+				</li>
+				<li class="jde-ui-item">
+					<div>
+						
+					</div>
+				</li>
+				
+			</ul>
 		</div>
 	</div>
     <script src="libs/bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="libs/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="libs/bower_components/pixi.js/dist/pixi.min.js"></script>
-	<script type="text/javascript" src="js/tara.js"></script>
+    <script>
+    	$(document).ready(function(){
+    		$('.jde-ui-item').click(function(){
+    			if(!$(this).hasClass('active'))
+    				$('.jde-ui-item.active').removeClass('active');
+    			$(this).toggleClass('active');
+    		});
+    	});
+    </script>
   </body>
 </html>
