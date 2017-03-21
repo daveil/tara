010a95b51943373ebf79d4b1df109b8a5bfbe18d
Dave Arroyo <arroyo.daveil@gmail.com>
2017-03-22 00:28:07 +0800

    Handle click event to open modal for adding item

--- index.php
+++ index.php
@@ -19,19 +19,19 @@
 				<li class="jde-ui-item">
 					<div class="jde-jewel">
 						<div class="jde-img jde-triangle"></div>
-						<button class="jde-btn btn btn-xs">+</button>
+						<button class="jde-btn btn btn-xs" data-toggle="modal" data-target="#JDEItemModal" data-item-code="1">+</button>
 					</div>
 				</li>
 				<li class="jde-ui-item">
 					<div class="jde-jewel">
 						<div class="jde-img jde-circle"></div>
-						<button class="jde-btn btn btn-xs">+</button>
+						<button class="jde-btn btn btn-xs" data-toggle="modal" data-target="#JDEItemModal" data-item-code="2">+</button>
 					</div>
 				</li>
 				<li class="jde-ui-item">
 					<div class="jde-jewel">
 						<div class="jde-img jde-square"></div>
-						<button class="jde-btn btn btn-xs">+</button>
+						<button class="jde-btn btn btn-xs" data-toggle="modal" data-target="#JDEItemModal" data-item-code="3">+</button>
 					</div>
 				</li>
 				<li class="jde-ui-item">
@@ -113,15 +113,49 @@
 			</div>
 		</div>
 	</div>
+	<div class="modal fade" tabindex="-1" role="dialog" id="JDEItemModal">
+	  <div class="modal-dialog" role="document">
+		<div class="modal-content">
+		  <div class="modal-body text-center">				
+				<h4 class="jde-name"></h4>
+				<img class="jde-image" src=""  />
+				<div class="jde-price">
+					<i>$<span></span></i>
+				</div>
+		  </div>
+		  <div class="modal-footer">
+			<button type="button" class="btn btn- pull-left" data-dismiss="modal">Close</button>
+			<button type="button" class="btn btn-primary">Confirm</button>
+			<div class="clear-fix"></div>
+		  </div>
+		</div><!-- /.modal-content -->
+	  </div><!-- /.modal-dialog -->
+	</div><!-- /.modal -->
     <script src="libs/bower_components/jquery/dist/jquery.min.js"></script>
     <script src="libs/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <script>
     	$(document).ready(function(){
+			var items = {
+				1:{name:'Triangle Object', img:'img/triangle.png',price:25},
+				2:{name:'Circle Object', img:'img/circle.png', price:20},
+				3:{name:'Square Object', img:'img/square.png', price:15},
+			};
     		$('.jde-ui-item').click(function(){
     			if(!$(this).hasClass('active'))
     				$('.jde-ui-item.active').removeClass('active');
     			$(this).toggleClass('active');
     		});
+			$('#JDEItemModal').on('show.bs.modal', function (event) {
+				 var button = $(event.relatedTarget)
+				 var itemCode =  button.data('item-code');
+				 var item = items[itemCode];
+				 var modal = $(this);
+				 modal.find('.jde-name').text(item.name);
+				 modal.find('.jde-image').attr('src',item.img);
+				 modal.find('.jde-price span').text(item.price);
+				 
+				 
+			});
     	});
     </script>
   </body>
