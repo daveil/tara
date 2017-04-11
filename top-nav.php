0c67bfc8f29edef23eb28440c1e904ae25c31825
Dave Arroyo <arroyo.daveil@gmail.com>
2017-04-11 09:57:29 +0800

    Add top-nav

---
+++ top-nav.php
@@ -0,0 +1,28 @@
+<nav id="top-nav" class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+	<div class="navbar-header">
+	  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+	  <div class="navbar-brand-wrapper">
+		  <a class="navbar-brand" href="#">
+			<span>
+				<img class="img-responsive" src="img/logo.png" alt="TARA" />
+			</span>
+		  </a>
+	  </div>
+	</div>
+	<div id="navbar" class="navbar-collapse collapse">
+	  
+	  <ul class="nav navbar-nav navbar-right">
+		<li><a href="#">SHOP</a></li>
+		<li><a href="#">ABOUT</a></li>
+		<li><a href="#">BLOG</a></li>
+		<li><a href="#">CONTACT</a></li>
+	  </ul>
+	</div><!--/.nav-collapse -->
+  </div>
+</nav>
