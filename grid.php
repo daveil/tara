706801cf77c198ca503f6214bf6641e1ba0b2618
Dave Alaras <arroyo.daveil@gmail.com>
2017-05-06 23:10:23 +0800

    New grid layout

---
+++ grid.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tara Jewelry Builder</title>
+	<link href="libs/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/grid.tara.css" />
+	<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+      <div class="grid-canvas vertical">
+        <div class="grid-container">
+            <div class="grid-item">
+              <div class="item">1</div>
+            </div>
+            <div class="grid-item tall">
+              <div class="item">2</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">3</div>
+            </div>
+            <div class="grid-item tall">
+              <div class="item">4</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">5</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">6</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">7</div>
+            </div>
+            <div class="grid-item tall">
+              <div class="item">8</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">9</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">10</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">11</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">12</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">13</div>
+            </div>
+          </div>
+        </div>
+        <div class="grid-canvas horizontal">
+            <div class="grid-container">
+              <div class="grid-item">
+                <div class="item">1</div>
+              </div>
+              <div class="grid-item">
+                <div class="item">2</div>
+              </div>
+              <div class="grid-item">
+                <div class="item">3</div>
+              </div>
+              <div class="grid-item">
+                <div class="item">4</div>
+              </div>
+              <div class="grid-item">
+                <div class="item">5</div>
+              </div>
+              <div class="grid-item">
+                <div class="item">6</div>
+              </div>
+              <div class="grid-item">
+                <div class="item">7</div>
+              </div>
+              <div class="grid-item">
+                <div class="item">8</div>
+              </div>
+              <div class="grid-item">
+                <div class="item">9</div>
+              </div>
+              <div class="grid-item">
+                <div class="item">10</div>
+              </div>
+              <div class="grid-item">
+                <div class="item">11</div>
+              </div>
+              <div class="grid-item">
+                <div class="item">12</div>
+              </div>
+              <div class="grid-item">
+                <div class="item">13</div>
+              </div>
+            </div>
+        </div>
+    </div>
+    <script src="libs/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="libs/bower_components/masonry/dist/masonry.pkgd.min.js"></script>
+    <script src="libs/bower_components/isotope/dist/isotope.pkgd.min.js"></script>
+    <script src="libs/bower_components/isotope-fit-columns/fit-columns.js"></script>
+    <script src="libs/bower_components/isotope-cells-by-column/cells-by-column.js"></script>
+    <script src="libs/bower_components/isotope-cells-by-row/cells-by-row.js"></script>
+    <script src="libs/bower_components/isotope-fit-columns/fit-columns.js"></script>
+    <script src="libs/bower_components/isotope-masonry-horizontal/masonry-horizontal.js"></script>
+    <script src="js/grid.tara.js"></script>
+  </body>
+</html>
