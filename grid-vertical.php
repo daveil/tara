9089b9e5147a082bdc0805306508141001f804a9
Dave Arroyo <arroyo.daveil@gmail.com>
2017-05-13 03:44:36 +0800

    Merge ui components from grid masonry and isotope

---
+++ grid-vertical.php
@@ -0,0 +1,43 @@
+ <div class="grid-canvas vertical hidden-sm hidden-xs">
+        <div class="grid-container">
+            <div class="grid-item">
+              <div class="item">1</div>
+            </div>
+            <div class="grid-item tall">
+              <div class="item">2</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">3</div>
+            </div>
+            <div class="grid-item tall">
+              <div class="item">4</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">5</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">6</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">7</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">8</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">9</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">10</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">11</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">12</div>
+            </div>
+            <div class="grid-item">
+              <div class="item">13</div>
+            </div>
+        </div>
+      </div>
---
+++ list-horizontal.php
@@ -0,0 +1,76 @@
+<div class="jde-horizontal-wrapper visible-sm visible-xs">
+	<ul class="jde-ui-list">
+		<li class="jde-ui-item">
+			<div class="jde-jewel">
+				<div class="jde-img jde-triangle"></div>
+				<button class="jde-btn btn btn-xs" data-toggle="modal" data-target="#JDEItemModal" data-item-code="1">+</button>
+			</div>
+		</li>
+		<li class="jde-ui-item">
+			<div class="jde-jewel">
+				<div class="jde-img jde-circle"></div>
+				<button class="jde-btn btn btn-xs" data-toggle="modal" data-target="#JDEItemModal" data-item-code="2">+</button>
+			</div>
+		</li>
+		<li class="jde-ui-item">
+			<div class="jde-jewel">
+				<div class="jde-img jde-square"></div>
+				<button class="jde-btn btn btn-xs" data-toggle="modal" data-target="#JDEItemModal" data-item-code="3">+</button>
+			</div>
+		</li>
+		<li class="jde-ui-item">
+			<div class="jde-jewel">
+				<div class="jde-img"></div>
+				<button class="jde-btn btn btn-xs">+</button>
+			</div>
+		</li>
+		<li class="jde-ui-item">
+			<div class="jde-jewel">
+				<div class="jde-img"></div>
+				<button class="jde-btn btn btn-xs">+</button>
+			</div>
+		</li>
+		<li class="jde-ui-item">
+			<div class="jde-jewel">
+				<div class="jde-img"></div>
+				<button class="jde-btn btn btn-xs">+</button>
+			</div>
+		</li>
+		<li class="jde-ui-item">
+			<div class="jde-jewel">
+				<div class="jde-img"></div>
+				<button class="jde-btn btn btn-xs">+</button>
+			</div>
+		</li>
+		<li class="jde-ui-item">
+			<div class="jde-jewel">
+				<div class="jde-img"></div>
+				<button class="jde-btn btn btn-xs">+</button>
+			</div>
+		</li>
+		<li class="jde-ui-item">
+			<div class="jde-jewel">
+				<div class="jde-img"></div>
+				<button class="jde-btn btn btn-xs">+</button>
+			</div>
+		</li>
+		<li class="jde-ui-item">
+			<div class="jde-jewel">
+				<div class="jde-img"></div>
+				<button class="jde-btn btn btn-xs">+</button>
+			</div>
+		</li>
+		<li class="jde-ui-item">
+			<div class="jde-jewel">
+				<div class="jde-img"></div>
+				<button class="jde-btn btn btn-xs">+</button>
+			</div>
+		</li>
+		<li class="jde-ui-item">
+			<div class="jde-jewel">
+				<div class="jde-img"></div>
+				<button class="jde-btn btn btn-xs">+</button>
+			</div>
+		</li>
+	</ul>
+</div>
