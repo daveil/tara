a1e97a2b229cba1d3a2ea69c7f9e5c0ec80a7079
Dave Alaras <arroyo.daveil@gmail.com>
2017-06-19 15:52:45 +0800

    Update pricing

--- js/build.tara.js
+++ js/build.tara.js
@@ -18,21 +18,21 @@ $(document).ready(function(){
 	const ENDING =  'E';
 	const TALL =  'T';
 	const BASES = {
-		1:{name:'Mari', slug:'mari', price:25,width:800,height:782, type:'T'},
-		3:{name:'Kimberly', slug:'kimberly', price:25,width:800,height:782,type:'T'},
-		2:{name:'Blake', slug:'blake', price:25,width:800,height:782, type:'T'},
+		1:{name:'Mari', slug:'mari', price:2088,width:800,height:782, type:'T'},
+		3:{name:'Kimberly', slug:'kimberly', price:288,width:800,height:782,type:'T'},
+		2:{name:'Blake', slug:'blake', price:758,width:800,height:782, type:'T'},
 	};
 	const PENDANTS = {
-		1:{name:'Celine', slug:'celine', price:25,width:67,height:89,altHeight:73,type:'R'},
-		2:{name:'Ciara', slug:'ciara', price:20,width:45,height:188,altHeight:168,type:'E'},
-		3:{name:'Rachel', slug:'rachel',  price:15,width:91,height:251,altHeight:233,type:'E'},
-		4:{name:'Attachment', slug:'attachment8', price:15,width:100,height:156,altHeight:139,type:'R'},
-		5:{name:'Diana', slug:'diana',  price:15,width:55,height:229,altHeight:213,type:'R'},
-		6:{name:'Emma', slug:'emma',  price:15,width:155,height:225,altHeight:207,type:'R'},
-		7:{name:'Kelly', slug:'kelly',  price:15,width:93,height:242,altHeight:224,type:'R'},
-		8:{name:'Sam', slug:'sam',  price:15,width:66,height:192,altHeight:173,type:'E'},
-		9:{name:'Martine', slug:'martine',  price:15,width:119,height:229,altHeight:214,type:'R'},
-		10:{name:'Mari', slug:'mari',  price:15,width:54,height:68,altHeight:68,type:'R'}
+		1:{name:'Celine', slug:'celine', price:898,width:67,height:89,altHeight:73,type:'R'},
+		2:{name:'Ciara', slug:'ciara', price:728,width:45,height:188,altHeight:168,type:'E'},
+		3:{name:'Rachel', slug:'rachel',  price:928,width:91,height:251,altHeight:233,type:'E'},
+		4:{name:'Grace', slug:'attachment8', price:1188,width:100,height:156,altHeight:139,type:'R'},
+		5:{name:'Diana', slug:'diana',  price:708,width:55,height:229,altHeight:213,type:'R'},
+		6:{name:'Emma', slug:'emma',  price:1198,width:155,height:225,altHeight:207,type:'R'},
+		7:{name:'Kelly', slug:'kelly',  price:1158,width:93,height:242,altHeight:224,type:'R'},
+		8:{name:'Sam', slug:'sam',  price:1293,width:66,height:192,altHeight:173,type:'E'},
+		9:{name:'Martine', slug:'martine',  price:988,width:119,height:229,altHeight:214,type:'R'},
+		10:{name:'Mari', slug:'mari',  price:2088,width:54,height:68,altHeight:68,type:'R'}
 	};
 	const APP = new PIXI.Application(WIDTH, HEIGHT, {backgroundColor : 0xffffff});
 	const MAX_ATTCH = 3;
