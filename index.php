f305ef32db80427a5c2bf445437081d8601e25c8
Dave Alaras <arroyo.daveil@gmail.com>
2017-06-25 20:56:22 +0800

    Adjust close button placement

--- index.php
+++ index.php
@@ -172,8 +172,9 @@
 		  <div class="modal-body" id="jde-summary">
 		  	<div class="row">
 				<div class="col-md-12">
-				<h2 class="text-left pull-left">Summary of your custom jewelry</h2>
-				 <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+				<h2 class="text-left">Summary of your custom jewelry</h2>
+				 
 				<div class="clearfix"></div>
 				<table class="table">
 					<thead>
