52ab07edb3550515e6d2cb8cdae9aa590da24d34
Dave Arroyo <arroyo.daveil@gmail.com>
2017-10-07 17:15:50 +0800

    Correct index from 5 to 6

--- js/app/controllers/jewel-base.js
+++ js/app/controllers/jewel-base.js
@@ -5,7 +5,7 @@ define(['app','f-ldr'],function(app,fldr){
 		2:{id: 2, name:'Blake', slug:'blake', price:758,width:160,height:156, type:'B', group:'E'},
 		4:{id: 4, name:'Choker', slug:'chocker', price:2288,width:1051,height:257, type:'B', soldAsPiece:true,group:'N'},
 		5:{id: 5, name:'Heart 3', slug:'heart3', price:2288,width:160,height:156, type:'B',group:'E'},
-		5:{id: 6, name:'6', slug:'6', price:2288,width:160,height:156, type:'B',group:'E'},
+		6:{id: 6, name:'6', slug:'6', price:2288,width:160,height:156, type:'B',group:'E'},
 	};
 	const OPTIONS = { "cellAlign": "center", "contain": true , "pageDots": false };
 	
