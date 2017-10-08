37721f8256b7cfc4fb4188bf6a9512bbcad8de07
Dave Arroyo <arroyo.daveil@gmail.com>
2017-10-08 13:52:56 +0800

    Launch codes

--- home.php
+++
@@ -1,8 +0,0 @@
-<?php include('elements/header.php')?>
-<?php include('top-nav.php')?>
-<?php include('elements/ui/landing.php')?>
-<?php include('elements/ui/type.php')?>
-<?php include('elements/ui/base.php')?>
-<?php include('elements/ui/build.php')?>
-<?php include('elements/ui/modal.php')?>
-<?php include('elements/footer.php')?>
